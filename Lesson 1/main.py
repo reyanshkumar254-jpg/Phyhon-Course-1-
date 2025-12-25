710b408d612361f392b2be189929dba5a4e776b5
Reyanshkumar1 <reyanshkumar254@gmail.com>
2025-12-25 16:29:53 +0530

    2nd Commit

---
+++ Lesson 1/main.py
@@ -0,0 +1,11 @@
+print("hello")
+print("Hi! I am Reyansh Kumar")
+print("34+34\n")
+print(24,48,96,42,sep=",",end="💖😂")
+variable="Hi! I am Reansh Kumar"
+print(variable)
+x= input("what is your date of birth?")
+print(x)
+import keyword 
+print(keyword.kwlist)
+print("Hi!,\n I am Reyansh Kumar",end="👌")
