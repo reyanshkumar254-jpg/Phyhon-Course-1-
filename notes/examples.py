868c1927498f2fd2185ba04b0eaeb2a2f8f9f577
Reyanshkumar1 <reyanshkumar254@gmail.com>
2025-12-27 08:31:04 +0530

    project.py(2)

---
+++ notes/examples.py
@@ -0,0 +1,12 @@
+#1st ex of \n command
+x= ("what is your name")
+print(x)
+#2nd ex of \n command
+x= ("what is \n your name")
+print(x)
+#input command ex 
+x=input("what is ypur name")
+print(x)
+#import keyword
+import keyword
+print(keyword.kwlist)
