0a54db9ff37b39d38a54d60cadf21bb86e4e64ab
Reyanshkumar1 <reyanshkumar254@gmail.com>
2025-12-27 09:00:47 +0530

    final project

--- lesson 2/project.py
+++ lesson 2/project.py
@@ -1,10 +1,34 @@
-neha= ("Neha=29th of june")
-print(neha)
-meher= ("Meher=17th of januaray")
-print(meher)
-rishab= ("Rishab=27 of october")
-print(rishab)
-abhay= ("Abhay=25th of december")
-print(abhay)
-pranav= ("Pranav=19th of july")
-print(pranav)
+name1= input("What is your name")
+
+age1= input("How old are you")
+
+name2= input("What is your name")
+
+age2= input("How old are you")
+
+name3= input("What is your name")
+
+age3= input("How old are you")
+
+name4= input("What is your name")
+
+age4= input("How old are you")
+
+name5= input("What is your name")
+
+age5= input("How old are you")
+
+print("name1 =", name1)
+print("age1 =", age1)
+
+print("name2 =", name2)
+print("age2 =", age2)
+
+print("name3 =", name3)
+print("age3 =", age3)
+
+print("name4 =", name4)
+print("age4 =", age4)
+
+print("name5 =", name5)
+print("age5 =", age5)
