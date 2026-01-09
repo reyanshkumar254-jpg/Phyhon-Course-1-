8098d56a3b704d0996d7c7cdf833afc58d015d4f
Reyanshkumar1 <reyanshkumar254@gmail.com>
2026-01-09 21:21:21 +0530

    5th commmit

---
+++ lesson 5/project.py
@@ -0,0 +1,9 @@
+temperature = int(input("What is the Temperature: "))
+
+if temperature > 30:
+    print("It is okay to wear Light Clothes.")
+elif temperature > 25:
+    print("You can wear Normal Clothes.")
+else:
+    print("It is not recommended to wear Light Clothes.")
+    print("You should wear Warm Clothes")
