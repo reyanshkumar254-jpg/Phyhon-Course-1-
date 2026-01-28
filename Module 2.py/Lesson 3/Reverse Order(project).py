30fe682acfdbf6e1f55dd15471f40ed02f32b471
Reyanshkumar1 <reyanshkumar254@gmail.com>
2026-01-28 16:10:01 +0530

    3rd project

---
+++ Module 2.py/Lesson 3/Reverse Order(project).py
@@ -0,0 +1,12 @@
+number =int(input("Enter a number- "))
+
+count =0
+
+if number == 0:
+    count = 1
+else:
+    while number != 0:
+        number = number // 10
+        count += 1
+
+print("Total number of digits:", count)
